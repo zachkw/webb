58a4e20f953afa9e3de558a3d6e5fe2d2ade8664
kinx <w.zacharyk@gmail.com>
2020-04-24 09:27:29 +0100

    added basic commands for full requests

--- insta/routes.js
+++ insta/routes.js
@@ -4,14 +4,35 @@ var service = require('./service')
   
 router.get('/', (req, res) => res.send('Hello IG World!'))
   
-router.get('/ig/:ig', async (req, res) => {
+router.get('/handle/:handle/open', async (req, res) => {
   try {
-    const result = await service.igScrape(req.params.ig);
+    const result = await service.handleScrape(req.params.handle);
+    res.send(result);
+  } 
+  catch(e) {
+    throw e;
+  }
+});
+
+router.get('/handle/:handle/text', async (req, res) => {
+  try {
+    const result = await service.handleTextScrape(req.params.handle);
     res.send(result);
   }
   catch(e) {
-    next(e);
+    throw e;
   }
 });
+
+router.get('/hashtag/:hashtag/open', async (req, res) => {
+  try {
+    const result = await service.hashtagScrape(req.params.hashtag);
+    res.send(result);
+  } catch(e) {
+    throw e;
+  }
+});
+
+
 
 module.exports = router
--- insta/service.js
+++ insta/service.js
@@ -1,11 +1,23 @@
 var ig = require('instagram-scraping');
 
+const handleScrape = async(handle) => {
+  let res = await ig.scrapeUserPage(handle);
+  return res;
+}
 
-const igScrape = async(igTag) => {
-  let res = await ig.scrapeUserPage(igTag);  
+const handleTextScrape = async(handle) => {
+  let res = await ig.scrapeUserPage(handle);  
   const mappedText = res.medias.map((instagramObject) => instagramObject.text);
   return mappedText;
 }
 
-exports.igScrape = igScrape;
+const hashtagScrape = async(name) => {
+  let res = await ig.scrapeTag(name);
+  return res;
+}
+
+
+exports.handleTextScrape = handleTextScrape;
+exports.handleScrape = handleScrape;
+exports.hashtagScrape = hashtagScrape;
 
