d5aca642b6cbb973e7bf586fdc9afb6f7bf20ebc
kinx <w.zacharyk@gmail.com>
2020-04-26 19:18:33 +0100

    added CORS TING

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const insta = require('./insta/routes')
 const google = require('./google/routes')
+const cors = require('cors');
 
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+
 app.use('/insta', insta);
 app.use('/google', google);
-
-const options = { headers: { 'content-type': 'application/json;charset=UTF-8', "Access-Control-Allow-Origin": "*" } };
-
 
 app.listen(PORT);
